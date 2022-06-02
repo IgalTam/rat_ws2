71c70e208975dae150acaba66b429a7a6bbb32b0
scole02 <samcole107@gmail.com>
2022-06-01 18:33:55 -0700

    final changes made during expo

--- src/rat_control/src/scripts/move_group_interface.py
+++ src/rat_control/src/scripts/move_group_interface.py
@@ -25,150 +25,6 @@ pose.position:
     z value inreases as claw gains elevation in relation to base 
 """
 
-# def inverseKinematics(x:float, z:float, phi_lo:int, phi_hi:int) -> tuple:
-#     # Length of links in cm
-#     l1 = BASE_TO_ELBOW; l2 = ELBOW_TO_WRIST; l3 = WRIST_TO_EEF
-
-#     # Desired Position of End effector
-#     px = x
-#     pz = z
-
-#     #phi = deg2rad(phi)
-#     # Equations for Inverse kinematics
-#     with warnings.catch_warnings():
-#         warnings.simplefilter('ignore') 
-#         for phi in range(phi_lo, phi_hi+1):
-#             phi = np.deg2rad(phi)
-#             wx = px - l3*cos(phi)
-#             wz = pz - l3*np.sin(phi)
-
-#             # delta = wx**2 + wz**2
-#             # c2 = ( delta -l1**2 -l2**2)/(2*l1*l2)
-#             # s2 = np.sqrt(1-c2**2)  # elbow down
-#             # theta_2 = np.arctan2(s2, c2)
-
-#             theta_2 = pi - np.arccos((l1**2 + l2**2 - wx**2 - wz**2)/(2*l1*l2))
-#             theta_1 = np.arctan2(wz,wx) \
-#                 - np.arccos((wx**2 + wz**2 + l1**2 - l2**2)/(2*l1*np.sqrt(wx**2 + wz**2)))
-#             # s1 = ((l1+l2*c2)*wz - l2*s2*wx)/delta
-#             # c1 = ((l1+l2*c2)*wx + l2*s2*wz)/delta
-#             # theta_1 = np.arctan2(s1,c1)
-#             theta_3 = phi-theta_1-theta_2
-
-#             gamma = np.arctan2(wz,wx) - theta_1
-#             theta_1_prime = theta_1 + 2*gamma
-#             theta_2_prime = -theta_2
-#             theta_3_prime = phi - theta_1_prime - theta_2_prime
-#             def check_joint_limits(t1, t2, t3):
-#                 if t1 > 0 or t1 < -3.2:
-#                     return False
-#                 elif t2 > 0 or t2 < -3.2:
-#                     return False
-#                 elif t3 > 3.2 or t3 < -3.2:
-#                     return False
-#                 return True
-#             theta_1_m_p = theta_1_prime - pi ; theta_2_m_p = -(theta_2_prime + pi); theta_3_m_p = theta_3_prime + pi/2
-#             theta_1_m = theta_1 - pi ; theta_2_m = -(theta_2 + pi); theta_3_m = theta_3+ pi/2
-
-#             #if(not np.isnan(theta_1) and check_joint_limits(theta_1_m_p, theta_2_m_p, theta_3_m_p)):
-#             #print(phi)
-#             if(not np.isnan(theta_1_prime)):
-#                 # print('theta_1: ', np.rad2deg(theta_1), theta_1)
-#                 # print('theta_2: ', np.rad2deg(theta_2), theta_2)
-#                 # print('theta_3: ', np.rad2deg(theta_3), theta_3)
-#                 # theta_1 = theta_1 - pi; theta_2 =  -(theta_2 - pi); theta_3 = theta_3 + pi/2
-#                 # print('theta_mapped1: ', np.rad2deg(theta_1), theta_1)
-#                 # print('theta_mapped2: ', np.rad2deg(theta_2), theta_2)
-#                 # print('theta_mapped3: ', np.rad2deg(theta_3), theta_3)
-#                 # print('theta_1_m:  ', np.rad2deg(theta_1_m), theta_1_m)
-#                 # print('theta_2_m:  ', np.rad2deg(theta_2_m), theta_2_m)
-#                 # print('theta_3_m:  ', np.rad2deg(theta_3_m), theta_3_m)
-
-#                 # print('\ntheta_prime_m1: ', np.rad2deg(theta_1_m_p), theta_1_m_p)
-#                 # print('theta_prime_m2: ', np.rad2deg(theta_2_m_p), theta_2_m_p)
-#                 # print('theta_prime_m3: ', np.rad2deg(theta_3_m_p), theta_3_m_p)
-#                 while theta_3_m_p > pi:
-#                     theta_3_m_p -= 2*pi
-#                 while theta_1_m > pi:
-#                     theta_1_m -= 2*pi
-#                 while theta_1_m < -pi:
-#                     theta_1_m += 2*pi
-
-#                 while theta_2_m > pi:
-#                     theta_2_m -= 2*pi
-#                 while theta_2_m < -pi:
-#                     theta_2_m += 2*pi
-#                 while theta_3_m > pi:
-#                     theta_3_m -= 2*pi
-#                 while theta_3_m < -pi:
-#                     theta_3_m += 2*pi
-
-#                 if(check_joint_limits(theta_1_m_p, theta_2_m_p, theta_3_m_p)):
-#                     return (theta_1_m_p, theta_2_m_p, theta_3_m_p), phi
-#                 elif(check_joint_limits(theta_1_m, theta_2_m, theta_3_m)):
-#                     return (theta_1_m, theta_2_m, theta_3_m), phi
-#     return None
-
-# def forwardKinematics(joint_vals: list, silent=False) -> tuple:
-#     # code here :
-#     #https://github.com/aakieu/3-dof-planar/blob/master/DHFowardKinematics.py
-    
-#     # dh parameters explained here:
-#     #https://blog.robotiq.com/how-to-calculate-a-robots-forward-kinematics-in-5-easy-steps
-
-#     # the distance between the previous x-axis and the current x-axis, along the previous z-axis.
-#     d1 = 0; d2 = 0; d3 = 0 # not actually zero from elbow to wrist
-
-#     # Angles in radians between links, each joint will need some remapping
-#     theta_1 = joint_vals[0] + pi
-#     theta_2 = -joint_vals[1] + pi
-#     theta_3 = joint_vals[2] - pi/2
-#     #theta_1 = 3.1415 ; theta_2 = 0 ; theta_3 = 0
-#     #print(f't1 {np.rad2deg(theta_1)} t2 {np.rad2deg(theta_2)} t3 {np.rad2deg(theta_3)}')
-#     # length of common normal, distance between prev z axis and cur z axis, along x axis
-#     r1 = BASE_TO_ELBOW; r2 = ELBOW_TO_WRIST; r3 = WRIST_TO_EEF
-
-#     # angle around common nomral between prev z and cur z
-#     alphl1 = 0; alphl2 = 0; alphl3 = 0
-
-#     # DH Parameter Table for 3 DOF Planar
-#     PT = [[theta_1, alphl1, r1, d1],
-#           [theta_2, alphl2, r2, d2],
-#           [theta_3, alphl3, r3, d3]]
-
-#     # Homogeneous Transformation Matrices
-#     i = 0
-#     H0_1 = [[cos(PT[i][0]), -np.sin(PT[i][0])*cos(PT[i][1]), np.sin(PT[i][0])*np.sin(PT[i][1]), PT[i][2]*cos(PT[i][0])],
-#             [np.sin(PT[i][0]), cos(PT[i][0])*cos(PT[i][1]), -cos(PT[i][0])*np.sin(PT[i][1]), PT[i][2]*np.sin(PT[i][0])],
-#             [0, np.sin(PT[i][1]), cos(PT[i][1]), PT[i][3]],
-#             [0, 0, 0, 1]]
-
-#     i = 1
-#     H1_2 = [[cos(PT[i][0]), -np.sin(PT[i][0])*cos(PT[i][1]), np.sin(PT[i][0])*np.sin(PT[i][1]), PT[i][2]*cos(PT[i][0])],
-#             [np.sin(PT[i][0]), cos(PT[i][0])*cos(PT[i][1]), -cos(PT[i][0])*np.sin(PT[i][1]), PT[i][2]*np.sin(PT[i][0])],
-#             [0, np.sin(PT[i][1]), cos(PT[i][1]), PT[i][3]],
-#             [0, 0, 0, 1]]
-
-#     i = 2
-#     H2_3 = [[cos(PT[i][0]), -np.sin(PT[i][0])*cos(PT[i][1]), np.sin(PT[i][0])*np.sin(PT[i][1]), PT[i][2]*cos(PT[i][0])],
-#             [np.sin(PT[i][0]), cos(PT[i][0])*cos(PT[i][1]), -cos(PT[i][0])*np.sin(PT[i][1]), PT[i][2]*np.sin(PT[i][0])],
-#             [0, np.sin(PT[i][1]), cos(PT[i][1]), PT[i][3]],
-#             [0, 0, 0, 1]]
-
-#     # print("H0_1 =")
-#     # print(np.matrix(H0_1))
-#     # print("H1_2 =")
-#     # print(np.matrix(H1_2))
-#     # print("H2_3 =")
-#     # print(np.matrix(H2_3))
-
-#     H0_2 = np.dot(H0_1,H1_2)
-#     H0_3 = np.dot(H0_2,H2_3)
-#     if not silent:
-#         print("H0_3 =")
-#         print(np.matrix(H0_3))
-#     return H0_3[0][3], H0_3[1][3] # x, z coords of eef
-
 class MoveGroupInterface(object):
     def __init__(self, silent=False):
         # super()
@@ -295,30 +151,31 @@ def main_interactive():
                 continue
             new_x = input("x: ")
             new_z = input("z: ")
-            new_phi_lo = input("phi low: ")
-            new_phi_hi = input("phi hi: ")
-
+            # new_phi_lo = input("phi low: ")
+            # new_phi_hi = input("phi hi: ")
+            phi_lo = 270
+            phi_hi = 280
 
             # check if input is int or float
 
-            if new_x[0] == '-' and new_x[1:].replace('.', '', 1).isdigit(): # used to handle negative values
+            if new_x and new_x[0] == '-' and new_x[1:].replace('.', '', 1).isdigit(): # used to handle negative values
                 x = float(new_x)
             elif new_x.replace('.', '', 1).isdigit(): 
                 x = float(new_x)
 
-            if new_z[0] == '-' and new_z[1:].replace('.', '', 1).isdigit(): # used to handle negative values
+            if new_z and new_z[0] == '-' and new_z[1:].replace('.', '', 1).isdigit(): # used to handle negative values
                 z = float(new_z)
             elif new_z.replace('.', '', 1).isdigit(): 
                 z = float(new_z)
 
-            if new_phi_lo.replace('.', '', 1).isdigit(): 
-                new_phi_lo = int(new_phi_lo)
-                if new_phi_lo >= 0 and new_phi_lo<= 360: # check its a valid angle
-                    phi_lo = int(new_phi_lo)
-            if new_phi_hi.replace('.', '', 1).isdigit(): 
-                new_phi_hi = int(new_phi_hi)
-                if new_phi_hi >= 0 and new_phi_hi <= 360:
-                    phi_hi = int(new_phi_hi)
+            # if new_phi_lo.replace('.', '', 1).isdigit(): 
+            #     new_phi_lo = int(new_phi_lo)
+            #     if new_phi_lo >= 0 and new_phi_lo<= 360: # check its a valid angle
+            #         phi_lo = int(new_phi_lo)
+            # if new_phi_hi.replace('.', '', 1).isdigit(): 
+            #     new_phi_hi = int(new_phi_hi)
+            #     if new_phi_hi >= 0 and new_phi_hi <= 360:
+            #         phi_hi = int(new_phi_hi)
             print(f'x: {x:0.5f} z: {z:0.5f} phi range: {phi_lo} - {phi_hi}')
             if input("Search for Joint Solution? (y/n): ") == 'y':
                 sol = inverseKinematics(x, z, phi_lo=phi_lo, phi_hi=phi_hi)
--- src/roboclaw_driver/scripts/subscriber.py
+++ src/roboclaw_driver/scripts/subscriber.py
@@ -48,7 +48,7 @@ class RoboclawNode:
         self.center_motors()
 
     def center_motors(self):
-        for i in range(self.num_joints):
+        for i in range(self.num_joints-1): # dont center claw
             address = int(self.joint_addresses[i]) # default: 0x80 or 128
             channel = int(self.joint_channels[i])
             cnts_per_rev = int(self.joint_cnts_per_rev[i])
@@ -71,9 +71,9 @@ class RoboclawNode:
     def callback(self, data):
         if data.position_rads[self.num_joints - 1] != 0: # check if claw needs to be actuated, hacky as urdf does not know about claw
             print("Actuating Claw...")
-            #address = 0x81 # int(self.joint_addresses[self.num_joints - 1])
-            self.rc.SpeedAccelDeccelPositionM1(129, 0, 100, 0, 58, 1)
-            time.sleep(0.2)
+            address = 0x81 # int(self.joint_addresses[self.num_joints - 1])
+            self.rc.SpeedAccelDeccelPositionM1(129, 0, 200, 0, 58, 1)
+            time.sleep(1)
             self.rc.SetEncM1(address, 0) # reset this encoder
             return
         # upon getting a msg, check if previous msg is the same
@@ -104,7 +104,7 @@ class RoboclawNode:
         # if input("Send commands to RoboClaws? (Yes/no): ") != "Yes":
         #     rospy.loginfo(rospy.get_caller_id() + "ignoring last message")
         #     return
-        for i in range(0, self.num_joints):
+        for i in range(0, self.num_joints-1):
             # fetch data from config file
             address = int(self.joint_addresses[i]) # default: 0x80 or 128
             channel = int(self.joint_channels[i])
@@ -116,21 +116,23 @@ class RoboclawNode:
             # accel = data.accel_deccel[i]
 
             if i == 1: # add elbow offset
-                rads -= 0.0698 # ~4 degrees
+                rads -= 0.0175 # ~1 degrees
 
 
             # Linux comport name
             #rc.Open()
-            
             flip_direction = int(self.joint_direction[i])
             buf = 1
             accel1 = 0
-            if i == 0:
-                speed1 = 300
-                speed2 = 300
-            else:
-                speed1 = 60
-                speed2 = 60
+            if i == 0: # base
+                speed1 = 370
+                speed2 = 370
+            elif i == 1: # elbow
+                speed1 = 115
+                speed2 = 115
+            else: # wrist
+                speed1 = 90
+                speed2 = 90
             deccel1 = 0
             cnts1 = 0
             accel2 = 0
@@ -141,7 +143,7 @@ class RoboclawNode:
 ## Doing this seperate based on the channels seems kind of dumb, but roboclaw lib
 ### uses different funcs for m1 and m2, there is a func for both m1 and m2 at the same time
 ### so that could replace this later
-
+            print(self.rc.ReadEncM2(128))
             if channel == 1:
                 # get current encoder val
                 cur_enc_val = self.rc.ReadEncM1(address)[1] # ReadEnc returns (success_code, enc_val, address)
@@ -154,16 +156,18 @@ class RoboclawNode:
                 # calculate encoder counts to write to motor
                 cnts1 = self.rads_to_enc_cnts(cnts_per_rev, rads)
                 if flip_direction:
-                        print(f'precnts1:{cnts1}')
+                        #print(f'precnts1:{cnts1}')
                         cnts1 = (2*cnts_per_rev) - cnts1
-                        print(f'postcnts1:{cnts1}')
+                        #print(f'postcnts1:{cnts1}')
                 if cnts1 == cur_enc_val:
                     rospy.loginfo(f'{cnts1} == {cur_enc_val}')
                     continue # dont need to write val if motor is already there
                 self.rc.SpeedAccelDeccelPositionM1(address,accel1,speed1,deccel1,cnts1,buf)
                 rospy.loginfo(f'Writing {cnts1}')
+
             elif channel == 2:
                 # get current encoder val
+                print(self.rc.ReadEncM2(address))
                 cur_enc_val = self.rc.ReadEncM2(address)[1]
                 cur_angle = ((cur_enc_val-cnts_per_rev)/cnts_per_rev) * 2 * pi
                 rospy.loginfo(f'Motor {i} {cur_enc_val}')
@@ -171,9 +175,9 @@ class RoboclawNode:
                 telem_msg.angle[i] = cur_angle
                 cnts2 = self.rads_to_enc_cnts(cnts_per_rev, rads)
                 if flip_direction:
-                        print(f'precnts2:{cnts2}')
+                        #print(f'precnts2:{cnts2}')
                         cnts2 = (2*cnts_per_rev) - cnts2
-                        print(f'postcnts2:{cnts2}')
+                        #print(f'postcnts2:{cnts2}')
                 # calculate encoder counts to write to motor
                 
                 if cnts2 == cur_enc_val:
