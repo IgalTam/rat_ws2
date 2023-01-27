e1aaade4ea5b317819fb178c27bf9d9c26eb1cb8
scole02 <samcole107@gmail.com>
2023-01-26 18:14:34 -0800

    base hall effect code completed

--- src/roboclaw_driver/scripts/roboclaw_python/baseHE_testing.py
+++ src/roboclaw_driver/scripts/roboclaw_python/baseHE_testing.py
@@ -1,20 +1,68 @@
 # This file is for testing the hall effect for 
-# functionality through the raspberry pi to see if 
-# it even works
+# functionality through the ROBOCLAW using s4 and s5 pins
 
 #!/usr/bin/python3
 import time
 from sys import exit
 from roboclaw_3 import Roboclaw
 
-# set up raspberry pi
-import RPi.GPIO as gpio
-gpio.setmode(gpio.BCM)
-HALLPIN = 2
-gpio.setup(HALLPIN, gpio.IN)
 
-while (True):
-    if(gpio.input(HALLPIN) == False):
-        print("magnet detected")
-    else:
-        print(" ")
+# constants
+CLAW_FORWARDROT = 60
+CLAW_BACKWARDROT = 240
+ELBOW_FULLROT = 3250
+WRIST_FULLROT = 1000
+BASE_FULLROT = 15000
+FULLROT = 360
+
+ROBOCLAW_1 = 128
+ROBOCLAW_2 = 129
+
+def main ():
+
+    #Linux comport name -> for UART3 on the Nuada Pi4, this is ttyAMA1)
+    rc = Roboclaw("/dev/ttyAMA1", 115200)
+    print(f"port open is {rc.Open()}")
+
+    # these values are defaulted
+    accel = 0
+    speed = 200
+    deccel = 0
+    mod_settings = True
+
+    # setting s4 to default: off
+    rc.SetPinFunctions(ROBOCLAW_1, 0, 0, 0)
+    time.sleep(1)
+
+    # setting s4 to setting motor home (user)
+    rc.SetPinFunctions(ROBOCLAW_1, 0, 0x62, 0)
+    print("s4 set to 0x62")
+    time.sleep(1)
+
+    # check/print that s4 pin was set correctly
+    print(rc.ReadPinFunctions(129))
+    time.sleep(1)
+    print("here")
+
+    # check if in homed state by reading status of all pins 
+    # (using ReadError function from roboclaw library) and then
+    # doing a bitwise AND with the value desired (in this case 0x400000)
+    homed = ((rc.ReadError(ROBOCLAW_1)[1] & 0x400000) == 0x400000)
+
+    print("here1")
+    val = 0
+    # rc.SpeedAccelDeccelPositionM1(ROBOCLAW_1, accel, speed, deccel, val, 1)
+
+    while (homed != True):
+        val -= 20
+        rc.SpeedAccelDeccelPositionM1(ROBOCLAW_1, accel, speed, deccel, val, 1)
+        time.sleep(1)
+        
+        homed = ((rc.ReadError(ROBOCLAW_1)[1] & 0x400000) == 0x400000)
+        # time.sleep(1)
+
+    print("Base Homed")
+
+if __name__ == "__main__":
+    main()
+
