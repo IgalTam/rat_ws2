2102b61dc52e364188373dde93798958c2b6be3e
Ahren27 <ahren.dosanjh@gmail.com>
2023-01-29 23:09:19 -0800

    skeleton functions

--- src/roboclaw_driver/scripts/vision_system/vision_communication.py
+++ src/roboclaw_driver/scripts/vision_system/vision_communication.py
@@ -12,7 +12,7 @@ import numpy as np
 
 class Vision_Communication:
 
-    def __init__(self, data_packets, x, y, z, theta, xFlag, angle, zFlag):
+    def __init__(self, data_packets, x, y, z, theta, xFlag, angle, zFlag, setFlag):
         self.data_packets = data_packets
         self.x = x
         self.y = y
@@ -21,6 +21,7 @@ class Vision_Communication:
         self.xFlag = xFlag
         self.angle = angle
         self.zFlag = zFlag
+        self.setFlag = setFlag
         
     def parse_data(self, data_packets, x, y, z, theta):
         
@@ -40,7 +41,43 @@ class Vision_Communication:
             elif x < 0:
                 turn_direction = "right"
             
+            # Call function to calculate angle
+            # Tank Turn Function:
             # turn(turn_direction, angle)
             
         else:
             xFlag = True
+    
+    def distance_view(self, z, zFlag):
+        if z < 3 or z > 10:
+            zFlag = False
+
+            if z < 3:
+                # Calculate distance for going forward
+                move_distance = 3 - z;
+            elif z > 10:
+                # Calculate distance for going backward (negative)
+                move_distance = 10 - z;
+            
+            # Call function to move rover forward or backward
+            # move_straight(z)
+            
+        else:
+            zFlag = True
+    
+    def position_set(self, xFlag, zFlag, setFlag):
+        if (not xFlag) or (not zFlag):
+            # Power back on vision system
+            Vision_Communication.power_vision_system()
+        
+        else:
+            setFlag = True
+    
+    def power_vision_system(self):
+        # Power up vision system
+        x = 5
+    
+    def vision_system_ros(self, setFlag, y, z, theta):
+        if setFlag:
+            # Send y, z, and theta into ROS
+            x = 5
